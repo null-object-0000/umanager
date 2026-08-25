c6ad33cf6b1277406fe60083b2645eae7237fea3
null_object_0000 <nichangen@outlook.com>
2026-08-25 18:47:29 +0800

    chore: bump version to 0.2.1

---
+++ qq_config.js
@@ -0,0 +1,7 @@
+
+      ;(function(){var params= {"version":"3.2.32","x64DownloadUrl":{"deb":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_amd64_01.deb","appimage":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_x86_64_01.AppImage","rpm":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_x86_64_01.rpm"},"armDownloadUrl":{"deb":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_arm64_01.deb","rpm":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_aarch64_01.rpm","appimage":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_arm64_01.AppImage"},"updateDate":"2026-08-24","loongarchDownloadUrl":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_loongarch64_01.deb","mipsDownloadUrl":"https://qqdl.gtimg.cn/qqfile/QQNTV2/9.9.33/release/3f89efc5/QQ_3.2.32_260812_mips64el_01.deb"};
+      typeof callback_1888_config1 === "function" && callback_1888_config1(params)
+      this.define && define(function (require, exports, module) {
+      return params});
+      })()
+    
